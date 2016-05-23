78d6cbba52f51d02a0bd53c571c3319fdd0b386c
Thejas Nair <thejas@hortonworks.com>
2016-05-23 10:30:07 -0700

    BUG-58222: fix mysql upgrade for v1.2.1000 upgrades

--- metastore/scripts/upgrade/mysql/upgrade-1.2.0-to-1.2.1000.mysql.sql
+++ metastore/scripts/upgrade/mysql/upgrade-1.2.0-to-1.2.1000.mysql.sql
@@ -1,6 +1,5 @@
-SELECT 'Upgrading MetaStore schema from 1.2.0 to 2.0.0' AS ' ';
-SOURCE 021-HIVE-7018.mysql.sql;
-SOURCE 022-HIVE-11970.mysql.sql;
+SELECT 'Upgrading MetaStore schema from 1.2.0 to 1.2.1000' AS ' ';
+
 SOURCE 023-HIVE-12807.mysql.sql;
 SOURCE 024-HIVE-12814.mysql.sql;
 SOURCE 025-HIVE-12816.mysql.sql;
@@ -12,6 +11,6 @@ SOURCE 030-HIVE-12823.mysql.sql;
 SOURCE 031-HIVE-12831.mysql.sql;
 SOURCE 032-HIVE-12832.mysql.sql;
 
-UPDATE VERSION SET SCHEMA_VERSION='2.0.0', VERSION_COMMENT='Hive release version 2.0.0' where VER_ID=1;
-SELECT 'Finished upgrading MetaStore schema from 1.2.0 to 2.0.0' AS ' ';
+UPDATE VERSION SET SCHEMA_VERSION='1.2.1000', VERSION_COMMENT='Hive release version 1.2.1000' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 1.2.0 to 1.2.1000' AS ' ';
 
