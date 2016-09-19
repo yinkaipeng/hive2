0b7681e2f29e60b9fcca592f4c7101dbcef74cf3
Rajesh Balamohan <rbalamohan@apache.org>
2016-09-19 21:42:24 +0530

    BUG-64011. Backport (erie) HIVE-14480: ORC ETLSplitStrategy should use thread pool when computing splits

--- ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcInputFormat.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcInputFormat.java
@@ -1110,7 +1110,7 @@ public class OrcInputFormat implements InputFormat<NullWritable, OrcStruct>,
     }
 
     public boolean isBlocking() {
-      return ppdResult != null;
+      return true;
     }
 
     Path getPath() {
