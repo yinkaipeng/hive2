18ffa725f337bf2973ca7a74a4a2e26732fd74a3
Sergey Shelukhin <sershe@apache.org>
2016-12-02 11:25:16 -0800

    HIVE-15312 : reduce logging in certain places (Sergey Shelukhin, reviewed by Prasanth Jayachandran)
    Conflicts:
    	ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
    	ql/src/java/org/apache/hadoop/hive/ql/io/AcidUtils.java
    	ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcInputFormat.java

--- ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
@@ -2976,6 +2976,7 @@ public final class Utilities {
 
       // The alias may not have any path
       Path path = null;
+      boolean hasLogged = false;
       for (String file : new LinkedList<String>(work.getPathToAliases().keySet())) {
         List<String> aliases = work.getPathToAliases().get(file);
         if (aliases.contains(alias)) {
@@ -2989,12 +2990,17 @@ public final class Utilities {
 
           pathsProcessed.add(path);
 
-          LOG.info("Adding input file " + path);
+          if (LOG.isDebugEnabled()) {
+            LOG.debug("Adding input file " + path);
+          } else if (!hasLogged) {
+            hasLogged = true;
+            LOG.info("Adding " + work.getPathToAliases().size()
+                + " inputs; the first input is " + path);
+          }
           if (!skipDummy
               && isEmptyPath(job, path, ctx)) {
             path = createDummyFileForEmptyPartition(path, job, work,
                  hiveScratchDir, alias, sequenceNumber++);
-
           }
           pathsToAdd.add(path);
         }
