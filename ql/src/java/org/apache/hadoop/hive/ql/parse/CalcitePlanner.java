7e4030b624f65e9f51302127a6d5b9db4ff6a7ac
Vineet Garg <vgarg@hortonworks.com>
2017-02-03 18:58:00 -0800

    HIVE-15458 : Fix semi-join conversion rule for subquery (Vineet Garg via Ashutosh Chauhan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>
    
    Conflicts:
    	ql/src/test/results/clientpositive/llap/dynamic_partition_pruning.q.out
    	ql/src/test/results/clientpositive/llap/lineage3.q.out
    	ql/src/test/results/clientpositive/llap/multiMapJoin2.q.out
    	ql/src/test/results/clientpositive/llap/subquery_exists.q.out
    	ql/src/test/results/clientpositive/llap/subquery_in.q.out
    	ql/src/test/results/clientpositive/llap/subquery_multi.q.out
    	ql/src/test/results/clientpositive/llap/subquery_notin.q.out
    	ql/src/test/results/clientpositive/llap/subquery_scalar.q.out
    	ql/src/test/results/clientpositive/llap/subquery_views.q.out
    	ql/src/test/results/clientpositive/llap/vector_mapjoin_reduce.q.out
    	ql/src/test/results/clientpositive/llap/vectorized_dynamic_partition_pruning.q.out
    	ql/src/test/results/clientpositive/perf/query70.q.out
    	ql/src/test/results/clientpositive/spark/subquery_in.q.out
    	ql/src/test/results/clientpositive/spark/vector_mapjoin_reduce.q.out
    	ql/src/test/results/clientpositive/vector_mapjoin_reduce.q.out
    
    Change-Id: I118db646bce2975caeef475977ff2c35b5085a04

--- ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
@@ -138,8 +138,17 @@ import org.apache.hadoop.hive.ql.log.PerfLogger;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.metadata.Table;
 import org.apache.hadoop.hive.ql.metadata.VirtualColumn;
-import org.apache.hadoop.hive.ql.optimizer.calcite.*;
+import org.apache.hadoop.hive.ql.optimizer.calcite.CalciteSemanticException;
+import org.apache.hadoop.hive.ql.optimizer.calcite.CalciteSubquerySemanticException;
 import org.apache.hadoop.hive.ql.optimizer.calcite.CalciteSemanticException.UnsupportedFeature;
+import org.apache.hadoop.hive.ql.optimizer.calcite.HiveCalciteUtil;
+import org.apache.hadoop.hive.ql.optimizer.calcite.HiveDefaultRelMetadataProvider;
+import org.apache.hadoop.hive.ql.optimizer.calcite.HivePlannerContext;
+import org.apache.hadoop.hive.ql.optimizer.calcite.HiveRelFactories;
+import org.apache.hadoop.hive.ql.optimizer.calcite.HiveRexExecutorImpl;
+import org.apache.hadoop.hive.ql.optimizer.calcite.HiveTypeSystemImpl;
+import org.apache.hadoop.hive.ql.optimizer.calcite.RelOptHiveTable;
+import org.apache.hadoop.hive.ql.optimizer.calcite.TraitsUtil;
 import org.apache.hadoop.hive.ql.optimizer.calcite.cost.HiveAlgorithmsConf;
 import org.apache.hadoop.hive.ql.optimizer.calcite.cost.HiveVolcanoPlanner;
 import org.apache.hadoop.hive.ql.optimizer.calcite.reloperators.HiveAggregate;
@@ -313,10 +322,6 @@ public class CalcitePlanner extends SemanticAnalyzer {
         disableJoinMerge = true;
         boolean reAnalyzeAST = false;
 
-        // currently semi-join optimization doesn't work with subqueries
-        // so this will be turned off for if we find subqueries and will later be
-        // restored to its original state
-        boolean originalSemiOptVal = this.conf.getBoolVar(ConfVars.SEMIJOIN_CONVERSION);
         try {
           if (this.conf.getBoolVar(HiveConf.ConfVars.HIVE_CBO_RETPATH_HIVEOP)) {
             sinkOp = getOptimizedHiveOPDag();
@@ -407,8 +412,6 @@ public class CalcitePlanner extends SemanticAnalyzer {
             super.genResolvedParseTree(ast, new PlannerContext());
             skipCalcitePlan = true;
           }
-          // restore semi-join opt flag
-          this.conf.setBoolVar(ConfVars.SEMIJOIN_CONVERSION, originalSemiOptVal);
         }
       } else {
         this.ctx.setCboInfo("Plan not optimized by CBO.");
@@ -2293,9 +2296,6 @@ public class CalcitePlanner extends SemanticAnalyzer {
                 .get(srcRel));
         relToHiveRR.put(filterRel, relToHiveRR.get(srcRel));
         this.subqueryId++;
-
-        // semi-join opt doesn't work with subqueries
-        conf.setBoolVar(ConfVars.SEMIJOIN_CONVERSION, false);
         return filterRel;
       } else {
         return genFilterRelNode(searchCond, srcRel, outerNameToPosMap, outerRR, forHavingClause);
