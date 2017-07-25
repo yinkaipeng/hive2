993cb5b70269c2557e364014b8e55d7fd33fb2cb
Pengcheng Xiong <pxiong@apache.org>
2017-07-25 15:41:14 -0700

    BUG-87107 : Backport ndv bit vector related changes RMP-1857 : Improve stats combining HIVE-16997: Extend object store to store and use bit vectors (Pengcheng Xiong, reviewed by Ashutosh Chauhan)
    Change-Id: I0b1d85fc547b4c2419c67337d9d9f1525a54fa74

---
+++ metastore/scripts/upgrade/derby/upgrade-2.1.1000-to-2.1.2000.derby.sql
@@ -0,0 +1,5 @@
+-- Upgrade MetaStore schema from 2.1.1000 to 2.1.2000
+
+RUN '044-HIVE-16997.derby.sql';
+
+UPDATE "APP".VERSION SET SCHEMA_VERSION='2.1.2000', VERSION_COMMENT='Hive release version 2.1.2000' where VER_ID=1;
