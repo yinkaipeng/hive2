4a143ac300b73b38343cc36ce27d6d48336ded62
Ashutosh Chauhan <hashutosh@apache.org>
2017-08-02 08:58:40 -0700

    BUG-87107 : Backport ndv bit vector related changes RMP-1857 : Improve stats combining HIVE-17190 : Schema changes for bitvectors for unpartitioned tables
    Change-Id: Ied427ae33e6fcd47761c80903ad1f40bd9b4db3e

--- metastore/scripts/upgrade/azuredb/upgrade-2.1.1000-to-2.1.2000.azuredb.sql
+++ metastore/scripts/upgrade/azuredb/upgrade-2.1.1000-to-2.1.2000.azuredb.sql
@@ -2,6 +2,7 @@ SELECT 'Upgrading MetaStore schema from 2.1.1000 to 2.1.2000' AS MESSAGE;
 
 /****** Add column for bit vector in PART_COL_STATS ******/
 ALTER TABLE [dbo].[PART_COL_STATS] ADD [BIT_VECTOR] [varbinary](max) NULL;
+ALTER TABLE [dbo].[TAB_COL_STATS] ADD [BIT_VECTOR] [varbinary](max) NULL;
 
 UPDATE [dbo].[VERSION] SET SCHEMA_VERSION='2.1.2000', VERSION_COMMENT='Hive release version 2.1.2000' where VER_ID=1;
 SELECT 'Finished upgrading MetaStore schema from 2.1.1000 to 2.1.2000' AS MESSAGE;
