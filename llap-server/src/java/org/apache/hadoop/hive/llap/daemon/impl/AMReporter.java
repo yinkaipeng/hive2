difin/difin_hive

HIVE-15554 : Add task information to LLAP AM heartbeat (Sergey Shelukhin, reviewed by Siddharth Seth)

llap-server/src/java/org/apache/hadoop/hive/llap/daemon/impl/AMReporter.java
@@ -14,6 +14,20 @@
 
 package org.apache.hadoop.hive.llap.daemon.impl;
 
+import org.apache.hadoop.hive.llap.protocol.LlapTaskUmbilicalProtocol.TezAttemptArray;
+
+import org.apache.hadoop.io.ArrayWritable;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+import java.util.HashSet;
+
+import java.util.Set;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 import javax.net.SocketFactory;
 
 import java.io.IOException;
@@ -175,7 +189,8 @@ public class AMReporter extends AbstractService {
   }
 
   public void registerTask(String amLocation, int port, String user,
-                           Token<JobTokenIdentifier> jobToken, QueryIdentifier queryIdentifier) {
+      Token<JobTokenIdentifier> jobToken, QueryIdentifier queryIdentifier,
+      TezTaskAttemptID attemptId) {
     if (LOG.isTraceEnabled()) {
       LOG.trace("Registering for heartbeat: " + amLocation + ":" + port + " for queryIdentifier=" + queryIdentifier);
     }
@@ -184,9 +199,8 @@ public class AMReporter extends AbstractService {
       LlapNodeId amNodeId = LlapNodeId.getInstance(amLocation, port);
       amNodeInfo = knownAppMasters.get(amNodeId);
       if (amNodeInfo == null) {
-        amNodeInfo =
-            new AMNodeInfo(amNodeId, user, jobToken, queryIdentifier, retryPolicy, retryTimeout, socketFactory,
-                conf);
+        amNodeInfo = new AMNodeInfo(amNodeId, user, jobToken, queryIdentifier,
+            retryPolicy, retryTimeout, socketFactory, conf);
         knownAppMasters.put(amNodeId, amNodeInfo);
         // Add to the queue only the first time this is registered, and on
         // subsequent instances when it's taken off the queue.
@@ -194,11 +208,11 @@ public class AMReporter extends AbstractService {
         pendingHeartbeatQueeu.add(amNodeInfo);
       }
       amNodeInfo.setCurrentQueryIdentifier(queryIdentifier);
-      amNodeInfo.incrementAndGetTaskCount();
+      amNodeInfo.addTaskAttempt(attemptId);
     }
   }
 
-  public void unregisterTask(String amLocation, int port) {
+  public void unregisterTask(String amLocation, int port, TezTaskAttemptID ta) {
     if (LOG.isTraceEnabled()) {
       LOG.trace("Un-registering for heartbeat: " + amLocation + ":" + port);
     }
@@ -209,7 +223,7 @@ public class AMReporter extends AbstractService {
       if (amNodeInfo == null) {
         LOG.info(("Ignoring duplicate unregisterRequest for am at: " + amLocation + ":" + port));
       } else {
-        amNodeInfo.decrementAndGetTaskCount();
+        amNodeInfo.removeTaskAttempt(ta);
       }
       // Not removing this here. Will be removed when taken off the queue and discovered to have 0
       // pending tasks.
@@ -334,29 +348,33 @@ public class AMReporter extends AbstractService {
       if (LOG.isTraceEnabled()) {
         LOG.trace("Attempting to heartbeat to AM: " + amNodeInfo);
       }
-      if (amNodeInfo.getTaskCount() > 0) {
-        try {
-          if (LOG.isTraceEnabled()) {
-            LOG.trace("NodeHeartbeat to: " + amNodeInfo);
-          }
-          amNodeInfo.getUmbilical().nodeHeartbeat(new Text(nodeId.getHostname()),
-              new Text(daemonId.getUniqueNodeIdInCluster()), nodeId.getPort());
-        } catch (IOException e) {
-          QueryIdentifier currentQueryIdentifier = amNodeInfo.getCurrentQueryIdentifier();
-          amNodeInfo.setAmFailed(true);
-          LOG.warn("Failed to communicated with AM at {}. Killing remaining fragments for query {}",
-              amNodeInfo.amNodeId, currentQueryIdentifier, e);
-          queryFailedHandler.queryFailed(currentQueryIdentifier);
-        } catch (InterruptedException e) {
-          if (!isShutdown.get()) {
-            LOG.warn("Interrupted while trying to send heartbeat to AM {}", amNodeInfo.amNodeId, e);
-          }
-        }
-      } else {
+      List<TezTaskAttemptID> tasks = amNodeInfo.getTasksSnapshot();
+      if (tasks.isEmpty()) {
         if (LOG.isDebugEnabled()) {
           LOG.debug("Skipping node heartbeat to AM: " + amNodeInfo + ", since ref count is 0");
         }
+        return null;
       }
+      try {
+        if (LOG.isTraceEnabled()) {
+          LOG.trace("NodeHeartbeat to: " + amNodeInfo);
+        }
+        TezAttemptArray aw = new TezAttemptArray();
+        aw.set(tasks.toArray(new TezTaskAttemptID[tasks.size()]));
+        amNodeInfo.getUmbilical().nodeHeartbeat(new Text(nodeId.getHostname()),
+            new Text(daemonId.getUniqueNodeIdInCluster()), nodeId.getPort(), aw);
+      } catch (IOException e) {
+        QueryIdentifier currentQueryIdentifier = amNodeInfo.getCurrentQueryIdentifier();
+        amNodeInfo.setAmFailed(true);
+        LOG.warn("Failed to communicated with AM at {}. Killing remaining fragments for query {}",
+            amNodeInfo.amNodeId, currentQueryIdentifier, e);
+        queryFailedHandler.queryFailed(currentQueryIdentifier);
+      } catch (InterruptedException e) {
+        if (!isShutdown.get()) {
+          LOG.warn("Interrupted while trying to send heartbeat to AM {}", amNodeInfo.amNodeId, e);
+        }
+      }
+
       return null;
     }
   }
@@ -364,7 +382,8 @@ public class AMReporter extends AbstractService {
 
 
   private static class AMNodeInfo implements Delayed {
-    private final AtomicInteger taskCount = new AtomicInteger(0);
+    // Serves as lock for itself.
+    private final Set<TezTaskAttemptID> tasks = new HashSet<>();
     private final String user;
     private final Token<JobTokenIdentifier> jobToken;
     private final Configuration conf;
@@ -422,12 +441,22 @@ public class AMReporter extends AbstractService {
       umbilical = null;
     }
 
-    int incrementAndGetTaskCount() {
-      return taskCount.incrementAndGet();
+    int addTaskAttempt(TezTaskAttemptID attemptId) {
+      synchronized (tasks) {
+        if (!tasks.add(attemptId)) {
+          throw new RuntimeException(attemptId + " was already registered");
+        }
+        return tasks.size();
+      }
     }
 
-    int decrementAndGetTaskCount() {
-      return taskCount.decrementAndGet();
+    int removeTaskAttempt(TezTaskAttemptID attemptId) {
+      synchronized (tasks) {
+        if (!tasks.remove(attemptId)) {
+          throw new RuntimeException(attemptId + " was not registered and couldn't be removed");
+        }
+        return tasks.size();
+      }
     }
 
     void setAmFailed(boolean val) {
@@ -438,8 +467,12 @@ public class AMReporter extends AbstractService {
       return amFailed.get();
     }
 
-    int getTaskCount() {
-      return taskCount.get();
+    List<TezTaskAttemptID> getTasksSnapshot() {
+      List<TezTaskAttemptID> result = new ArrayList<>();
+      synchronized (tasks) {
+        result.addAll(tasks);
+      }
+      return result;
     }
 
     public synchronized QueryIdentifier getCurrentQueryIdentifier() {
@@ -474,6 +507,12 @@ public class AMReporter extends AbstractService {
     @Override
     public String toString() {
       return "AMInfo: " + amNodeId + ", taskCount=" + getTaskCount();
+    }
+
+    private int getTaskCount() {
+      synchronized (tasks) {
+        return tasks.size();
+      }
     }
   }
 }

llap-server/src/java/org/apache/hadoop/hive/llap/daemon/impl/TaskRunnerCallable.java
@@ -141,7 +141,7 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
     // Register with the AMReporter when the callable is setup. Unregister once it starts running.
     if (amReporter != null && jobToken != null) {
       this.amReporter.registerTask(request.getAmHost(), request.getAmPort(),
-          vertex.getUser(), jobToken, fragmentInfo.getQueryInfo().getQueryIdentifier());
+          vertex.getUser(), jobToken, fragmentInfo.getQueryInfo().getQueryIdentifier(), attemptId);
     }
     this.metrics = metrics;
     this.requestId = taskSpec.getTaskAttemptID().toString();
@@ -172,11 +172,12 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
       }
 
       // Unregister from the AMReporter, since the task is now running.
-      this.amReporter.unregisterTask(request.getAmHost(), request.getAmPort());
+      TezTaskAttemptID ta = taskSpec.getTaskAttemptID();
+      this.amReporter.unregisterTask(request.getAmHost(), request.getAmPort(), ta);
 
       synchronized (this) {
         if (!shouldRunTask) {
-          LOG.info("Not starting task {} since it was killed earlier", taskSpec.getTaskAttemptID());
+          LOG.info("Not starting task {} since it was killed earlier", ta);
           return new TaskRunner2Result(EndReason.KILL_REQUESTED, null, null, false);
         }
       }
@@ -290,19 +291,19 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
     if (!isCompleted.get()) {
       if (!killInvoked.getAndSet(true)) {
         synchronized (this) {
-          LOG.info("Kill task requested for id={}, taskRunnerSetup={}", taskSpec.getTaskAttemptID(),
-              (taskRunner != null));
+          TezTaskAttemptID ta = taskSpec.getTaskAttemptID();
+          LOG.info("Kill task requested for id={}, taskRunnerSetup={}", ta, taskRunner != null);
           if (taskRunner != null) {
             killtimerWatch.start();
             LOG.info("Issuing kill to task {}", taskSpec.getTaskAttemptID());
             boolean killed = taskRunner.killTask();
             if (killed) {
               // Sending a kill message to the AM right here. Don't need to wait for the task to complete.
-              LOG.info("Kill request for task {} completed. Informing AM", taskSpec.getTaskAttemptID());
+              LOG.info("Kill request for task {} completed. Informing AM", ta);
               reportTaskKilled();
             } else {
               LOG.info("Kill request for task {} did not complete because the task is already complete",
-                  taskSpec.getTaskAttemptID());
+                  ta);
             }
             shouldRunTask = false;
           } else {
@@ -314,7 +315,7 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
             // If the task hasn't started - inform about fragment completion immediately. It's possible for
             // the callable to never run.
             fragmentCompletionHanler.fragmentComplete(fragmentInfo);
-            this.amReporter.unregisterTask(request.getAmHost(), request.getAmPort());
+            this.amReporter.unregisterTask(request.getAmHost(), request.getAmPort(), ta);
           }
         }
       } else {

yinkaipeng/hive2

llap-server/src/java/org/apache/hadoop/hive/llap/daemon/impl/AMReporter.java
@@ -19,8 +19,12 @@ import javax.net.SocketFactory;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.security.PrivilegedExceptionAction;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.Delayed;
@@ -30,7 +34,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import com.google.common.util.concurrent.FutureCallback;
@@ -47,6 +50,7 @@ import org.apache.hadoop.hive.llap.DaemonId;
 import org.apache.hadoop.hive.llap.LlapNodeId;
 import org.apache.hadoop.hive.llap.daemon.QueryFailedHandler;
 import org.apache.hadoop.hive.llap.protocol.LlapTaskUmbilicalProtocol;
+import org.apache.hadoop.hive.llap.protocol.LlapTaskUmbilicalProtocol.TezAttemptArray;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.retry.RetryPolicies;
 import org.apache.hadoop.io.retry.RetryPolicy;
@@ -184,7 +188,8 @@ public class AMReporter extends AbstractService {
   }
 
   public void registerTask(String amLocation, int port, String umbilicalUser,
-                           Token<JobTokenIdentifier> jobToken, QueryIdentifier queryIdentifier) {
+      Token<JobTokenIdentifier> jobToken, QueryIdentifier queryIdentifier,
+      TezTaskAttemptID attemptId) {
     if (LOG.isTraceEnabled()) {
       LOG.trace(
           "Registering for heartbeat: {}, queryIdentifier={}",
@@ -198,9 +203,8 @@ public class AMReporter extends AbstractService {
       LlapNodeId amNodeId = LlapNodeId.getInstance(amLocation, port);
       amNodeInfo = knownAppMasters.get(queryIdentifier);
       if (amNodeInfo == null) {
-        amNodeInfo =
-            new AMNodeInfo(amNodeId, umbilicalUser, jobToken, queryIdentifier, retryPolicy, retryTimeout, socketFactory,
-                conf);
+        amNodeInfo = new AMNodeInfo(amNodeId, umbilicalUser, jobToken, queryIdentifier,
+            retryPolicy, retryTimeout, socketFactory, conf);
         knownAppMasters.put(queryIdentifier, amNodeInfo);
         // Add to the queue only the first time this is registered, and on
         // subsequent instances when it's taken off the queue.
@@ -211,11 +215,11 @@ public class AMReporter extends AbstractService {
         // A single queueLookupCallable is added here. We have to make sure one instance stays
         // in the queue till the query completes.
       }
-      amNodeInfo.incrementAndGetTaskCount();
+      amNodeInfo.addTaskAttempt(attemptId);
     }
   }
 
-  public void unregisterTask(String amLocation, int port, QueryIdentifier queryIdentifier) {
+  public void unregisterTask(String amLocation, int port, QueryIdentifier queryIdentifier, TezTaskAttemptID ta) {
     if (LOG.isTraceEnabled()) {
       LOG.trace("Un-registering for heartbeat: {}", (amLocation + ":" + port));
     }
@@ -225,7 +229,7 @@ public class AMReporter extends AbstractService {
       if (amNodeInfo == null) {
         LOG.info(("Ignoring duplicate unregisterRequest for am at: " + amLocation + ":" + port));
       } else {
-        amNodeInfo.decrementAndGetTaskCount();
+        amNodeInfo.removeTaskAttempt(ta);
       }
       // Not removing this here. Will be removed when taken off the queue and discovered to have 0
       // pending tasks.
@@ -379,25 +383,32 @@ public class AMReporter extends AbstractService {
       if (LOG.isTraceEnabled()) {
         LOG.trace("Attempting to heartbeat to AM: " + amNodeInfo);
       }
-      if (amNodeInfo.getTaskCount() > 0) {
-        try {
-          if (LOG.isTraceEnabled()) {
-            LOG.trace("NodeHeartbeat to: " + amNodeInfo);
-          }
-          amNodeInfo.getUmbilical().nodeHeartbeat(new Text(nodeId.getHostname()),
-              new Text(daemonId.getUniqueNodeIdInCluster()), nodeId.getPort());
-        } catch (IOException e) {
-          QueryIdentifier currentQueryIdentifier = amNodeInfo.getQueryIdentifier();
-          amNodeInfo.setAmFailed(true);
-          LOG.warn("Failed to communicated with AM at {}. Killing remaining fragments for query {}",
-              amNodeInfo.amNodeId, currentQueryIdentifier, e);
-          queryFailedHandler.queryFailed(currentQueryIdentifier);
-        } catch (InterruptedException e) {
-          if (!isShutdown.get()) {
-            LOG.warn("Interrupted while trying to send heartbeat to AM {}", amNodeInfo.amNodeId, e);
-          }
+      List<TezTaskAttemptID> tasks = amNodeInfo.getTasksSnapshot();
+      if (tasks.isEmpty()) {
+        if (LOG.isDebugEnabled()) {
+          LOG.debug("Skipping node heartbeat to AM: " + amNodeInfo + ", since ref count is 0");
         }
-      } 
+        return null;
+      }
+      try {
+        if (LOG.isTraceEnabled()) {
+          LOG.trace("NodeHeartbeat to: " + amNodeInfo);
+        }
+        TezAttemptArray aw = new TezAttemptArray();
+        aw.set(tasks.toArray(new TezTaskAttemptID[tasks.size()]));
+        amNodeInfo.getUmbilical().nodeHeartbeat(new Text(nodeId.getHostname()),
+            new Text(daemonId.getUniqueNodeIdInCluster()), nodeId.getPort(), aw);
+      } catch (IOException e) {
+        QueryIdentifier currentQueryIdentifier = amNodeInfo.getQueryIdentifier();
+        amNodeInfo.setAmFailed(true);
+        LOG.warn("Failed to communicated with AM at {}. Killing remaining fragments for query {}",
+            amNodeInfo.amNodeId, currentQueryIdentifier, e);
+        queryFailedHandler.queryFailed(currentQueryIdentifier);
+      } catch (InterruptedException e) {
+        if (!isShutdown.get()) {
+          LOG.warn("Interrupted while trying to send heartbeat to AM {}", amNodeInfo.amNodeId, e);
+        }
+      }
       return null;
     }
   }
@@ -405,7 +416,8 @@ public class AMReporter extends AbstractService {
 
 
   private static class AMNodeInfo implements Delayed {
-    private final AtomicInteger taskCount = new AtomicInteger(0);
+    // Serves as lock for itself.
+    private final Set<TezTaskAttemptID> tasks = new HashSet<>();
     private final String umbilicalUser;
     private final Token<JobTokenIdentifier> jobToken;
     private final Configuration conf;
@@ -464,12 +476,22 @@ public class AMReporter extends AbstractService {
       umbilical = null;
     }
 
-    int incrementAndGetTaskCount() {
-      return taskCount.incrementAndGet();
+    int addTaskAttempt(TezTaskAttemptID attemptId) {
+      synchronized (tasks) {
+        if (!tasks.add(attemptId)) {
+          throw new RuntimeException(attemptId + " was already registered");
+        }
+        return tasks.size();
+      }
     }
 
-    int decrementAndGetTaskCount() {
-      return taskCount.decrementAndGet();
+    int removeTaskAttempt(TezTaskAttemptID attemptId) {
+      synchronized (tasks) {
+        if (!tasks.remove(attemptId)) {
+          throw new RuntimeException(attemptId + " was not registered and couldn't be removed");
+        }
+        return tasks.size();
+      }
     }
 
     void setAmFailed(boolean val) {
@@ -484,12 +506,16 @@ public class AMReporter extends AbstractService {
       isDone.set(val);
     }
 
-    boolean isDone() {
-      return isDone.get();
+    List<TezTaskAttemptID> getTasksSnapshot() {
+      List<TezTaskAttemptID> result = new ArrayList<>();
+      synchronized (tasks) {
+        result.addAll(tasks);
+      }
+      return result;
     }
 
-    int getTaskCount() {
-      return taskCount.get();
+    boolean isDone() {
+      return isDone.get();
     }
 
     public QueryIdentifier getQueryIdentifier() {
@@ -520,6 +546,12 @@ public class AMReporter extends AbstractService {
     @Override
     public String toString() {
       return "AMInfo: " + amNodeId + ", taskCount=" + getTaskCount() + ", queryIdentifier=" + queryIdentifier;
+    }
+
+    private int getTaskCount() {
+      synchronized (tasks) {
+        return tasks.size();
+      }
     }
   }
 }

llap-server/src/java/org/apache/hadoop/hive/llap/daemon/impl/TaskRunnerCallable.java
@@ -151,7 +151,7 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
     // Register with the AMReporter when the callable is setup. Unregister once it starts running.
     if (amReporter != null && jobToken != null) {
       this.amReporter.registerTask(request.getAmHost(), request.getAmPort(),
-          vertex.getTokenIdentifier(), jobToken, fragmentInfo.getQueryInfo().getQueryIdentifier());
+          vertex.getTokenIdentifier(), jobToken, fragmentInfo.getQueryInfo().getQueryIdentifier(), attemptId);
     }
     this.metrics = metrics;
     this.requestId = taskSpec.getTaskAttemptID().toString();
@@ -188,12 +188,13 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
       }
 
       // Unregister from the AMReporter, since the task is now running.
+      TezTaskAttemptID ta = taskSpec.getTaskAttemptID();
       this.amReporter.unregisterTask(request.getAmHost(), request.getAmPort(),
-          fragmentInfo.getQueryInfo().getQueryIdentifier());
+          fragmentInfo.getQueryInfo().getQueryIdentifier(), ta);
 
       synchronized (this) {
         if (!shouldRunTask) {
-          LOG.info("Not starting task {} since it was killed earlier", taskSpec.getTaskAttemptID());
+          LOG.info("Not starting task {} since it was killed earlier", ta);
           return new TaskRunner2Result(EndReason.KILL_REQUESTED, null, null, false);
         }
       }
@@ -348,7 +349,7 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
             boolean killed = taskRunner.killTask();
             if (killed) {
               // Sending a kill message to the AM right here. Don't need to wait for the task to complete.
-              LOG.info("Kill request for task {} completed. Informing AM", taskSpec.getTaskAttemptID());
+              LOG.info("Kill request for task {} completed. Informing AM", ta);
               // Inform the scheduler that this fragment has been killed.
               // If the kill failed - that means the task has already hit a final condition,
               // and a notification comes from the LlapTaskReporter
@@ -356,7 +357,7 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
               reportTaskKilled();
             } else {
               LOG.info("Kill request for task {} did not complete because the task is already complete",
-                  taskSpec.getTaskAttemptID());
+                  ta);
             }
           } else {
             // If the task hasn't started, and it is killed - report back to the AM that the task has been killed.
@@ -367,9 +368,8 @@ public class TaskRunnerCallable extends CallableWithNdc<TaskRunner2Result> {
             // If the task hasn't started - inform about fragment completion immediately. It's possible for
             // the callable to never run.
             fragmentCompletionHanler.fragmentComplete(fragmentInfo);
-            this.amReporter
-                .unregisterTask(request.getAmHost(), request.getAmPort(),
-                    fragmentInfo.getQueryInfo().getQueryIdentifier());
+            this.amReporter.unregisterTask(request.getAmHost(), request.getAmPort(),
+                fragmentInfo.getQueryInfo().getQueryIdentifier(), ta);
           }
         }
       } else {