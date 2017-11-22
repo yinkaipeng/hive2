d78a404f21beac733d893e8a2d53a6f8acbf68ca
Zoltan Haindrich <kirk@rxd.hu>
2017-11-22 09:23:46 +0100

    BUG-111855 HIVE-18105: Aggregation of an empty set doesn't pass constants to the UDAF (Zoltan Haindrich, reviewed by Ashutosh Chauhan)

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDAFEvaluator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDAFEvaluator.java
@@ -33,9 +33,9 @@ import org.apache.hive.common.util.AnnotationUtils;
 /**
  * A Generic User-defined aggregation function (GenericUDAF) for the use with
  * Hive.
- * 
+ *
  * New GenericUDAF classes need to inherit from this GenericUDAF class.
- * 
+ *
  * The GenericUDAF are superior to normal UDAFs in the following ways: 1. It can
  * accept arguments of complex types, and return complex types. 2. It can accept
  * variable length of arguments. 3. It can accept an infinite number of function
@@ -105,7 +105,7 @@ public abstract class GenericUDAFEvaluator implements Closeable {
 
   /**
    * Initialize the evaluator.
-   * 
+   *
    * @param m
    *          The mode of aggregation.
    * @param parameters
@@ -117,7 +117,7 @@ public abstract class GenericUDAFEvaluator implements Closeable {
    *         mode, the ObjectInspector for the return value of
    *         terminatePartial() call; In FINAL and COMPLETE mode, the
    *         ObjectInspector for the return value of terminate() call.
-   * 
+   *
    *         NOTE: We need ObjectInspector[] (in addition to the TypeInfo[] in
    *         GenericUDAFResolver) for 2 reasons: 1. ObjectInspector contains
    *         more information than TypeInfo; and GenericUDAFEvaluator.init at
@@ -134,16 +134,17 @@ public abstract class GenericUDAFEvaluator implements Closeable {
   /**
    * The interface for a class that is used to store the aggregation result
    * during the process of aggregation.
-   * 
+   *
    * We split this piece of data out because there can be millions of instances
    * of this Aggregation in hash-based aggregation process, and it's very
    * important to conserve memory.
-   * 
+   *
    * In the future, we may completely hide this class inside the Evaluator and
    * use integer numbers to identify which aggregation we are looking at.
    *
    * @deprecated use {@link AbstractAggregationBuffer} instead
    */
+  @Deprecated
   public static interface AggregationBuffer {
   };
 
@@ -171,13 +172,14 @@ public abstract class GenericUDAFEvaluator implements Closeable {
    * Close GenericUDFEvaluator.
    * This is only called in runtime of MapRedTask.
    */
+  @Override
   public void close() throws IOException {
   }
 
   /**
    * This function will be called by GroupByOperator when it sees a new input
    * row.
-   * 
+   *
    * @param agg
    *          The object to store the aggregation result.
    * @param parameters
@@ -195,7 +197,7 @@ public abstract class GenericUDAFEvaluator implements Closeable {
   /**
    * This function will be called by GroupByOperator when it sees a new input
    * row.
-   * 
+   *
    * @param agg
    *          The object to store the aggregation result.
    */
@@ -209,7 +211,7 @@ public abstract class GenericUDAFEvaluator implements Closeable {
 
   /**
    * Iterate through original data.
-   * 
+   *
    * @param parameters
    *          The objects of parameters.
    */
@@ -217,7 +219,7 @@ public abstract class GenericUDAFEvaluator implements Closeable {
 
   /**
    * Get partial aggregation result.
-   * 
+   *
    * @return partial aggregation result.
    */
   public abstract Object terminatePartial(AggregationBuffer agg) throws HiveException;
@@ -225,7 +227,7 @@ public abstract class GenericUDAFEvaluator implements Closeable {
   /**
    * Merge with partial aggregation result. NOTE: null might be passed in case
    * there is no input data.
-   * 
+   *
    * @param partial
    *          The partial aggregation result.
    */
@@ -233,7 +235,7 @@ public abstract class GenericUDAFEvaluator implements Closeable {
 
   /**
    * Get final aggregation result.
-   * 
+   *
    * @return final aggregation result.
    */
   public abstract Object terminate(AggregationBuffer agg) throws HiveException;
@@ -248,7 +250,7 @@ public abstract class GenericUDAFEvaluator implements Closeable {
    * This method is called after this Evaluator is initialized. The returned
    * Function must be initialized. It is passed the 'window' of aggregation for
    * each row.
-   * 
+   *
    * @param wFrmDef
    *          the Window definition in play for this evaluation.
    * @return null implies that this fn cannot be processed in Streaming mode. So
