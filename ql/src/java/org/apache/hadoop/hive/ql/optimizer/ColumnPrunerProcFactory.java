570d9638564fbccc55ae3ac6944d7b84809a9bdd
Hengyu Dai <dhytheone@gmail.com>
2018-01-09 09:53:03 -0800

    BUG-102077 HIVE-18390: IndexOutOfBoundsException when query a  partitioned view in ColumnPruner (Hengyu Dai via Ashutosh Chauhan)

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/ColumnPrunerProcFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/ColumnPrunerProcFactory.java
@@ -28,6 +28,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
 
+import org.apache.hadoop.hive.metastore.api.FieldSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.hadoop.hive.ql.exec.AbstractMapJoinOperator;
@@ -789,8 +790,10 @@ public final class ColumnPrunerProcFactory {
         for (String col : cols) {
           int index = originalOutputColumnNames.indexOf(col);
           Table tab = cppCtx.getParseContext().getViewProjectToTableSchema().get(op);
+          List<FieldSchema> fullFieldList = new ArrayList<FieldSchema>(tab.getCols());
+          fullFieldList.addAll(tab.getPartCols());
           cppCtx.getParseContext().getColumnAccessInfo()
-              .add(tab.getCompleteName(), tab.getCols().get(index).getName());
+              .add(tab.getCompleteName(), fullFieldList.get(index).getName());
         }
       }
       if (cols.size() < originalOutputColumnNames.size()) {
