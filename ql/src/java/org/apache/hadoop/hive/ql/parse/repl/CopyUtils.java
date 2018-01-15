1473c68fa3804b9b55f1c1e9313164af5f5e01c8
Anishek Agarwal <anishek@gmail.com>
2018-01-15 10:43:55 +0530

    BUG-93806 / HIVE-18341: Add repl load support for adding "raw" namespace for TDE with same encryption keys (Anishek Agarwal, reviewed Daniel Dai)
    Change-Id: I99742dea692d326514522763006180ec664224b5

--- ql/src/java/org/apache/hadoop/hive/ql/parse/repl/CopyUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/repl/CopyUtils.java
@@ -18,6 +18,7 @@
 
 package org.apache.hadoop.hive.ql.parse.repl;
 
+import com.google.common.collect.Lists;
 import org.apache.hadoop.fs.ContentSummary;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
@@ -32,27 +33,30 @@ import org.apache.hadoop.security.UserGroupInformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Lists;
-
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
+import java.net.URI;
+import java.security.PrivilegedExceptionAction;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.apache.hadoop.hive.io.HdfsUtils.HadoopFileStatus;
 
 public class CopyUtils {
 
   private static final Logger LOG = LoggerFactory.getLogger(CopyUtils.class);
+  // https://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-hdfs/TransparentEncryption.html#Running_as_the_superuser
+  private static final String RAW_RESERVED_VIRTUAL_PATH = "/.reserved/raw/";
+  private static final int MAX_COPY_RETRY = 3;
 
   private final HiveConf hiveConf;
   private final long maxCopyFileSize;
   private final long maxNumberOfFiles;
   private final boolean hiveInTest, inheritPermissions;
   private final String copyAsUser;
-  private final int MAX_COPY_RETRY = 3;
 
   public CopyUtils(String distCpDoAsUser, HiveConf hiveConf) {
     this.hiveConf = hiveConf;
@@ -111,8 +115,7 @@ public class CopyUtils {
                            FileSystem destinationFs, Path destination,
                            boolean useRegularCopy) throws IOException, LoginException {
     int repeat = 0;
-    List<Path> pathList = Lists.transform(fileList,
-                                          fileInfo -> { return fileInfo.getEffectivePath(); });
+    List<Path> pathList = Lists.transform(fileList, ReplChangeManager.FileInfo::getEffectivePath);
     while (!pathList.isEmpty() && (repeat < MAX_COPY_RETRY)) {
       try {
         doCopyOnce(sourceFs, pathList, destinationFs, destination, useRegularCopy);
@@ -145,33 +148,74 @@ public class CopyUtils {
   private void doCopyOnce(FileSystem sourceFs, List<Path> srcList,
                           FileSystem destinationFs, Path destination,
                           boolean useRegularCopy) throws IOException, LoginException {
-    HadoopFileStatus sourceStatus =
-        new HadoopFileStatus(hiveConf, destinationFs, destination.getParent());
-
     UserGroupInformation ugi = Utils.getUGI();
     String currentUser = ugi.getShortUserName();
-    boolean usePrivilegedDistCp = copyAsUser != null && !currentUser.equals(copyAsUser);
+    boolean usePrivilegedUser = copyAsUser != null && !currentUser.equals(copyAsUser);
 
     if (useRegularCopy) {
-      Path[] paths = srcList.toArray(new Path[] {});
-      FileUtil.copy(sourceFs, paths, destinationFs, destination, false, true, hiveConf);
-      if (inheritPermissions) {
-        try {
-          HdfsUtils.setFullFileStatus(hiveConf, sourceStatus, destinationFs, destination, true);
-        } catch (Exception e) {
-          LOG.warn("Error setting permissions or group of " + destination, e);
-        }
+      doRegularCopyOnce(sourceFs, srcList, destinationFs, destination, usePrivilegedUser);
+    } else {
+      doDistCpCopyOnce(sourceFs, srcList, destination, usePrivilegedUser);
+    }
+  }
+
+  private void doDistCpCopyOnce(FileSystem sourceFs, List<Path> srcList, Path destination,
+      boolean usePrivilegedUser) throws IOException {
+    if (hiveConf.getBoolVar(HiveConf.ConfVars.REPL_ADD_RAW_RESERVED_NAMESPACE)) {
+      srcList = srcList.stream().map(path -> {
+        URI uri = path.toUri();
+        return new Path(uri.getScheme(), uri.getAuthority(),
+            RAW_RESERVED_VIRTUAL_PATH + uri.getPath());
+      }).collect(Collectors.toList());
+      URI destinationUri = destination.toUri();
+      destination = new Path(destinationUri.getScheme(), destinationUri.getAuthority(),
+          RAW_RESERVED_VIRTUAL_PATH + destinationUri.getPath());
+      hiveConf.set("distcp.options.px", "");
+    }
+
+    FileUtils.distCp(
+        sourceFs, // source file system
+        srcList,  // list of source paths
+        destination,
+        false,
+        usePrivilegedUser ? copyAsUser : null,
+        hiveConf,
+        ShimLoader.getHadoopShims()
+    );
+  }
+
+  private void doRegularCopyOnce(FileSystem sourceFs, List<Path> srcList, FileSystem destinationFs,
+      Path destination, boolean usePrivilegedUser) throws IOException {
+  /*
+    even for regular copy we have to use the same user permissions that distCp will use since
+    hive-server user might be different that the super user required to copy relevant files.
+   */
+    final Path[] paths = srcList.toArray(new Path[] {});
+    if (usePrivilegedUser) {
+      final Path finalDestination = destination;
+      UserGroupInformation proxyUser = UserGroupInformation.createProxyUser(
+          copyAsUser, UserGroupInformation.getLoginUser());
+      try {
+        proxyUser.doAs((PrivilegedExceptionAction<Boolean>) () -> {
+          FileUtil
+              .copy(sourceFs, paths, destinationFs, finalDestination, false, true, hiveConf);
+          return true;
+        });
+      } catch (InterruptedException e) {
+        throw new IOException(e);
       }
     } else {
-      FileUtils.distCp(
-              sourceFs, // source file system
-              srcList,  // list of source paths
-              destination,
-              false,
-              usePrivilegedDistCp ? copyAsUser : null,
-              hiveConf,
-              ShimLoader.getHadoopShims()
-      );
+      FileUtil.copy(sourceFs, paths, destinationFs, destination, false, true, hiveConf);
+    }
+
+    HadoopFileStatus sourceStatus =
+        new HadoopFileStatus(hiveConf, destinationFs, destination.getParent());
+    if (inheritPermissions) {
+      try {
+        HdfsUtils.setFullFileStatus(hiveConf, sourceStatus, destinationFs, destination, true);
+      } catch (Exception e) {
+        LOG.warn("Error setting permissions or group of " + destination, e);
+      }
     }
   }
 
@@ -181,8 +225,8 @@ public class CopyUtils {
 
     for (Map.Entry<FileSystem, List<Path>> entry : map.entrySet()) {
       final FileSystem sourceFs = entry.getKey();
-      List<ReplChangeManager.FileInfo> fileList = Lists.transform(entry.getValue(),
-                                path -> { return new ReplChangeManager.FileInfo(sourceFs, path);});
+      List<ReplChangeManager.FileInfo> fileList = entry.getValue().stream()
+          .map(path -> new ReplChangeManager.FileInfo(sourceFs, path)).collect(Collectors.toList());
       doCopyOnce(sourceFs, entry.getValue(),
                  destinationFs, destination,
                  regularCopy(destinationFs, sourceFs, fileList));
@@ -250,7 +294,7 @@ public class CopyUtils {
     for (Path path : srcPaths) {
       FileSystem fileSystem = path.getFileSystem(hiveConf);
       if (!result.containsKey(fileSystem)) {
-        result.put(fileSystem, new ArrayList<Path>());
+        result.put(fileSystem, new ArrayList<>());
       }
       result.get(fileSystem).add(path);
     }
@@ -258,12 +302,12 @@ public class CopyUtils {
   }
 
   private Map<FileSystem, List<ReplChangeManager.FileInfo>> fsToFileMap(
-      List<ReplChangeManager.FileInfo> srcFiles) throws IOException {
+      List<ReplChangeManager.FileInfo> srcFiles) {
     Map<FileSystem, List<ReplChangeManager.FileInfo>> result = new HashMap<>();
     for (ReplChangeManager.FileInfo file : srcFiles) {
       FileSystem fileSystem = file.getSrcFs();
       if (!result.containsKey(fileSystem)) {
-        result.put(fileSystem, new ArrayList<ReplChangeManager.FileInfo>());
+        result.put(fileSystem, new ArrayList<>());
       }
       result.get(fileSystem).add(file);
     }
