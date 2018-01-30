9b8b5fb17c94815b860e611c1c69dda698e6becf
Gopal V <gopalv@apache.org>
2018-01-29 23:41:26 -0800

    BUG-119783 HIVE-18574 : LLAP: Ship netty3 as part of LLAP install tarball (Gopal V via Ashutosh Chauhan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>
    (cherry picked from commit 7a04dd6269e0e26f8bddafd97d5a887ba0881e0c)
    
    Change-Id: I89485593245b4751a22f7de65ff9e5d1551cf56a

--- llap-server/src/java/org/apache/hadoop/hive/llap/cli/LlapServiceDriver.java
+++ llap-server/src/java/org/apache/hadoop/hive/llap/cli/LlapServiceDriver.java
@@ -389,7 +389,10 @@ public class LlapServiceDriver {
               org.apache.logging.log4j.core.Appender.class, // log4j-core
               org.apache.logging.slf4j.Log4jLogger.class, // log4j-slf4j
               // log4j-1.2-API needed for NDC
-              org.apache.log4j.config.Log4j1ConfigurationFactory.class, };
+              org.apache.log4j.config.Log4j1ConfigurationFactory.class,
+              io.netty.util.NetUtil.class, // netty4
+              org.jboss.netty.util.NetUtil.class //netty3
+              };
 
           for (Class<?> c : dependencies) {
             Path jarPath = new Path(Utilities.jarFinderGetJar(c));
