9e10c65093204998cb2a143e46e0c1c795b63c1a
Jesus Camacho Rodriguez <jcamacho@apache.org>
2019-01-31 09:27:47 -0800

    BUG-117460 HIVE-21188: SemanticException for query on view with masked table (Jesus Camacho Rodriguez, reviewed by Ashutosh Chauhan)
    Change-Id: I16102a674e68cee5c0409924a4b21d8647f463e6

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -11632,7 +11632,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       sb.append(" FROM (");
       sb.append(expandedText);
       sb.append(") ");
-      sb.append(HiveUtils.unparseIdentifier(createVwDesc.getViewName(), conf));
+      sb.append(HiveUtils.unparseIdentifier(Utilities.getDbTableName(createVwDesc.getViewName())[1], conf));
       expandedText = sb.toString();
     }
 
